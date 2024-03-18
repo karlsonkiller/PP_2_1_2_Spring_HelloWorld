c2e8f68905755896fdb1b86db485156c028918bc
karlsonkiller <sergss0213@yandex.ru>
2024-03-18 13:00:35 +0300

    do task

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat hellocat () {
+        Cat cat = new Cat();
+        cat.setMessage("Hello World!");
+        return cat;
+    }
 }
